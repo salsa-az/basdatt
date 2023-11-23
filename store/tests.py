6cadcf3ae41ba07ae5691e64fbbede7eb2069695
Salsabila <salsabilaazzhr@gmail.com>
2023-11-23 21:05:45 +0700

    adding models

--- store/tests.py
+++ store/tests.py
@@ -1,30 +1,3 @@
-from django.db import models
+from django.test import TestCase
 
 # Create your tests here.
-
-
-class Customer(models.Model):
-    username = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
-    
-    def __str__(self) -> str:
-        return self.name
-    
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
-    
-class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    date_ordered = models.DateTimeField(auto_now_add=True) # auto_now_add=True: set the date automatically when the order is created
-    complete = models.BooleanField(default=False)
-    transaction_id = models.CharField(max_length=200, null=True)
-    
-    def __str__(self) -> str:
-        return str(self.id)
-    
