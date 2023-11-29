5920584385d06f6c36a539f4eca9de7328d4f399
shyfakaay <shyfazulkifli@gmail.com>
2023-11-29 17:03:04 +0700

    COMMIT_EDITMSG_Punten paket

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 
 from .models import *
 
@@ -19,7 +20,7 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
        items = []
-       order = {'get_cart_total':0, 'get_cart_items':0}
+       order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
        cartItems = order['get_cart_items']
         
     context = {'items':items, 'order':order, 'cartItems': cartItems }
@@ -33,7 +34,7 @@ def checkout(request):
         cartItems = order.get_cart_items
     else:
        items = []
-       order = {'get_cart_total':0, 'get_cart_items':0}
+       order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
        cartItems = order['get_cart_items']
         
     context = {'items':items, 'order':order, 'cartItems':cartItems}
@@ -78,14 +79,44 @@ def face(request):
        order = {'get_cart_total':0, 'get_cart_items':0}
        cartItems = order['get_cart_items']
 
-    products = Product.objects.all()
-    context = {'products':products, 'cartItems': cartItems}
+    products = Product.objects.filter(category='face')
+    context = {'products': products}
     return render(request, 'store/face.html', context)
 
 def lip(request):
-    context = {}
+    products = Product.objects.filter(category='lip')
+    context = {'products': products}
     return render(request, 'store/lip.html', context)
 
 def eye(request):
-    context = {}
-    return render(request, 'store/eye.html', context)
+    products = Product.objects.filter(category='eye')
+    context = {'products': products}
+    return render(request, 'store/eye.html', context)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+    customer = request.user.Customer
+    order , created = Order.objects.get_or_create(customer=customer, complete=False)
+    total = float(data['from']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+
+    else:
+        print('User is not logged in')
+    return JsonResponse('Payment complete!, safe=False)
