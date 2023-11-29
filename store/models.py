50f8a807ebdf6d7f6201510d0556671283c9daf8
Salsabila <salsabilaazzhr@gmail.com>
2023-11-30 06:15:01 +0700

    branch

--- store/models.py
+++ store/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
     
     def __str__(self) -> str:
         return self.name
@@ -47,10 +47,10 @@ class Order(models.Model):
     def __str__(self) -> str:
         return str(self.id)
     
-    # @property
-    # def shipping(self):
-    #     shipping = True
-    #     return shipping
+    @property
+    def shipping(self):
+        shipping = True
+        return shipping
     
     @property
     def get_cart_total(self):
